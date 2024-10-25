e1ffbb616537d4bcab240cc9b24ce2c9685d2ef2
QAM <qam600@gmail.com>
2024-10-25 15:26:31 +0400

    files updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4173,
+    strictPort: true, // Prevents the server from using an alternative port
+  },
+  preview: {
+    port: 4173,
+  },
+  base: './',
 });
